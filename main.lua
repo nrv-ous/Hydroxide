f2902166258e88458d3ad2773b41ef5bb94aa490
nerve <38772631+0x90-NOP@users.noreply.github.com>
2019-06-08 19:36:27 -0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,361 @@
+--[[ <
+                                                                                                                                                               
+                                   **.     .*.                   ,&/.                              .(*         *%/                                             
+                                   &&/    ./&/                   *&/.                                          *&/                                             
+                                   &&/    .(&/ *#(    ./#. .*(%%//&/. .#/*%%*  .*#%%#,   (/.   (#. .#/   .*%%(/(&/   .*#%#(*                                   
+                                   &@%#####&@/  ,#%* .&%* *@(.   *&/. .@%,   ,@#,    %@#  /&@&&,   ,&(  *&(    *&/ .(&/,,,*#%,                                 
+                                   &&/    .(&/   .@%,(%*  /&/.   *&/. .,     ,@(.    #@#  ,#@@&    ,&(  \&/    *&/ .(&/,,,,,.                                  
+                                   ##*     *#*    ./@#     ,/#%%*,#*  .#(.     ,(%%%#.   #*    #%. .#/   ,(%%(**#*   ./%%%%#,                                  
+                                                  ,%#,                                                                                                         
+                                                  (&*                                                                                                           
+                                                                                                                                                               
+                                                                                                                                                               
+                                                                                                            ..,,,,,,,***********,,.                            
+                                                                                                      ...,,,,,,,,,,,,,,,,,,,,**********,.                      
+                                                                                                   .,,,,,,,,,,,,,,,,,,,,,,,,,,,,,**********,                   
+                                                                                               ,,,,,......................,,,,,,,,,,,,**********,              
+                                                                                             ,,,..............................,,,,,,,,,,***********,           
+                                                   .,,//(###%%%%%%%%%%%%%%%##((/*,.        .,,.................................,,,,,,,,,,************.         
+                                            .*/(#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%##(*,............              ............,,,,,,,,,,************,.       
+                                   ,/(%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%(*.......                        ..........,,,,,,,,,*************,    
+                                ./(%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%/,......                           ........,,,,,,,,,***********//**    
+                             ,*#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*......                             .......,,,,,,,,,************///,   
+                          ,/%%%%%%%%%%%%%%%%###############%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*.....                               ......,,,,,,,,,************///**,  
+                     ,(%%%%%%%%%%%###############################%%%%%%%%%%%%%%%%%%%%%%%*.....                               .......,,,,,,,,,************/////, 
+                   *(%%%%%%%%%%#####################################%#%%%%%%%%%%%%%%%%%%/,....                               ......,,,,,,,,,*************/////* 
+                 .*%%%%%%%%%#########################################%##%%%%%%%%%%%%%%%%(,....                               ......,,,,,,,,,*************////*,
+                (#%%%%%%%%################################################%%%%%%%%%%%%%%#/.....                             .......,,,,,,,,,*************/////*
+            ,/%%%%%%%#######################################################%%%%%%%%%%%%%%/,.....                         .......,,,,,,,,,,*************//////*
+           (%%%%%%%%#########################################################%%%%%%%%%%%%%#/,......                     ........,,,,,,,,,,**************//////*
+         ,(%%%%%#%##########################((((((((((((((######################%%%%%%%%%%%#(.........               .........,,,,,,,,,,***************///////*
+         (%%%%%%%#######################((((((((((((((((((((((################%%%%%%%%%%%%%%#*...........         ...........,,,,,,,,,,,**************////////*
+      .#%%%%%%####################((((/////////////////////////((((################%%%%%%%%%%%%/,......................,,,,,,,,,,,,,,***************/////////,.
+     ,(%%%%%%%##################((((//////////////*****///////////(((##############%%%%%%%%%%%%%(,,,...............,,,,,,,,,,,,,,,,*****************/////////. 
+    *#%%%%%%###################((((//////////**************////////(((#############%%%%%%%%%%%%%%#/,,,,,,,,,,,,,,,,,,,,,,,,,,,,,******************//////////*  
+   .#%%%%%%##################((((////////*********************///////((############%%%%%%%%%%%%%%%%#*,,,,,,,,,,,,,,,,,,,,,,,,,*******************//////////,.  
+  ,(%%%%%%%#################(((///////**************************//////((###########%%%%%%%%%%%%%%%%%%%(/*,,,,,,,,,,,,,,,***********************/////////**.    
+  (%%%%%%%#################((((///////***************************//////((##########%%%%%%%%%%%%%%%%%%%%%%/*,,,,,,,**************************///////////*.      
+ .%%%%%%%%%################(((///////*****************************/////((##########%%%%%%%%%%%%%%%%%%%%%%%%(******************************////////////*.       
+./%%%%%%%%#################(((//////******************************/////((##########%%%%%%%%%%%%%%%%%%%%%%%%%%#(/***********************/////////////*,         
+*#%%%%%%%%#################(((//////*****************************//////((##########%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#(/**********////////////////*,.            
+(%%%%%%%%%##################(((///////**************************//////((##########%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&%%#(///////////////////**,.               
+#%%%%%%%%%%#################(((///////**************************/////((##########%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&%%%##(((((//*,,.                    
+#%%%%%%%%%%##################(((////////**********************//////((###########%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&%/.                       
+%%%%%%%%%%%%%###################(((//////////*********//////////((############%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&%/.                       
+%%%%%%%%%%%%%####################((((/////////////////////////(((#############%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&&%/.                       
+#%%%%%%%%%%%%%%#####################((((///////////////////(((##############%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&&&%/.                       
+#%%%%%%%%%%%%%%%#######################((((((((/////(((((((################%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&&&*                        
+/%%%%%%%%%%%%%%%%%%####################################################%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&&&(.                        
+,#%%%%%%%%%%%%%%%%%%%%%############################################%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&&&&&&*                         
+./%%%%%%%%%%%%%%%%%%%%%%%#######################################%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&&&&&&%.                         
+ ,%%%%%%%%%%%%%%%%%%%%%%%#####################################%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&&&&&&                          
+  *#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%######%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&&&&&&&/.                          
+  ./%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&&&&&&&&%.                           
+   .#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&&&&&&&&%/                            
+    *#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&*                             
+      ,%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&%(                               
+       *#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&/.                               
+        .%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&%(                                 
+         ,(&&%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&%,                                  
+           .*#&%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&/.                                    
+             .#%&%&&&%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&*                                      
+               ,%&&&&&&%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&%(                                        
+                .*(&&&&&&&&%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&%(.                                         
+                     ,(%&&&&&&&&&&&&&&%%%%&&&&&&&&&&&%&&&&%%%%%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&%*.                                             
+                       /#%&&&&&&&&&&&&&&&&&&&&&%%%%%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&/,                                               
+                         .(#&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&%(,                                                  
+                            ,/#&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&%#,.                                                    
+                                   ,/(%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&%&&&%/,.                                                           
+                                       .*(%%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&%%#*,                                                                
+                                          .,*##%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&%%#(,.                                                                   
+                                                 .,*(#%%%&&&&&&&&&&&&&&&&&&&&%%%#(/,,                                                                          
+
+Hydroxide :: nerve
+    "Hydroxide is a penetration testing tool, used to edge the security of Roblox games."
+
+<< Features:
+    > Client Environment "Explorer"
+    > Data Manipulation
+    > Data Information
+    > Code Generation
+    > Client Script Viewer
+    > Remote Call "Spy"
+>>
+
+]]-- >
+
+-- < Delete Clones >
+if ui and game.CoreGui:FindFirstChild("Hydroxide") then
+    ui = nil
+    game.CoreGui.Hydroxide:Destroy()
+end
+
+-- < Environment Check >
+do
+    if not getgenv then
+        print("<OH-> : Your exploit does not have getgenv")
+        return
+    end
+
+    local verified = true
+    for i,v in next, {
+        getrawmetatable = getrawmetatable or debug.getmetatable,
+        setrawmetatable = setrawmetatable or debug.setmetatable,
+        getupvalues = debug.getupvalues or getupvalues or getupvals,
+        getupvalue = debug.getupvalue or getupvalue or getupval,
+        setupvalue = debug.setupvalue or setupvalue or setupval,
+        islclosure = islclosure,
+    } do
+        if v then
+            getgenv()[i] = v
+        else
+            verified = false
+            print("<OH-> : Your exploit dose not have " .. i)
+        end
+    end
+
+    if not verified then
+        warn("<OH-> : Make sure your exploit has all of the functions above, before you use Hydroxide!")
+        return
+    end
+end
+
+-- < Abstractions >
+local isEmpty = function(table)
+    local count = 0
+    for i,v in next, table do
+        count = count + 1
+    end
+    return count == 0
+end
+
+local isObject = function(check)
+    return type(check) ~= typeof(check)
+end
+
+-- < Variables >
+getgenv().ui = {}
+local interface = game:GetObjects("rbxassetid://3223506759")[1]
+local drag = interface.Drag
+local body = drag.Body
+local border = drag.BorderFrame
+local collapse = drag.Collapse
+
+local sidebar = body.Sidebar
+local buttonClone = sidebar._G:Clone()
+buttonClone.Icon.ImageTransparency = 0
+buttonClone.Label.Size = UDim2.new(1, -40, 0, 20)
+buttonClone.ClipsDescendants = true
+
+sidebar._G:Destroy()
+
+local information = body.Information
+local console = body.Console
+
+-- < Interface: Logo >
+drag.Logo.Image = "rbxassetid://3270632025"
+
+-- < Interface: Message Box >
+do
+    local messageBox = interface.MessageBox
+    messageBox.Button.MouseButton1Click:Connect(function()
+        messageBox.Visible = false
+
+        border.Visible = true
+    end)
+
+    ui.msg = false
+    setrawmetatable(ui.msg, {
+        __call = function(t, title, message)
+            messageBox.Visible = true
+            messageBox.Title.Text = title
+            messageBox.Content.Text = message
+            
+            border.Visible = false
+        end}) 
+end
+
+-- < Interface: Drag >
+do -- creds to @[ Tiffblocks ]
+    local dragging
+    local dragInput
+    local dragStart
+    local startPos
+
+    drag.InputBegan:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseButton1 then
+            dragging = true
+            dragStart = input.Position
+            startPos = drag.Position
+            
+            input.Changed:Connect(function()
+                if input.UserInputState == Enum.UserInputState.End then
+                    dragging = false
+                end
+            end)
+        end
+    end)
+
+    drag.InputChanged:connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseMovement then
+            dragInput = input
+        end
+    end)
+
+    game:getService("UserInputService").InputChanged:connect(function(input)
+        if input == dragInput and dragging then
+            local delta = input.Position - dragStart
+            drag.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+        end
+    end)
+end
+
+-- < Interface: Sidebar>
+local typeIcons = {
+    string = 3270578699,
+    number = 3270578699,
+    boolean = 3270578699,
+    table = 3270579576,
+    userdata = 3270579823,
+    ['function'] = 3270578480
+}
+
+local collapseIcon = {
+	["true"] = 3270754211,
+	["false"] = 3271004659
+}
+
+ui.findRoot = function(obj, root)
+	if type(obj) ~= 'userdata' then 
+		return
+	end
+
+	if obj.Parent == root then 
+		return obj
+	end
+
+	return ui.findRoot(obj.Parent, root)
+end
+
+ui.getMostX = function(elements)
+    local current = 0
+    for i,v in next, elements do
+        if not v:IsA("UIListLayout") and current < v.Position.X.Offset then
+            current = v
+        end
+    end
+    return current
+end
+
+ui.addButton = function(name, data, parent, showCollapse)
+    local dataType = type(data)
+
+    local button = buttonClone:Clone()
+    local collapse = button.Collapse
+
+    local children 
+
+    if not parent then
+        parent = sidebar
+    end
+
+    button.Parent = parent
+    button.Name = name
+    button.Label.Text = name
+    button.Icon.Image = "rbxassetid://" .. typeIcons[type(data)]
+
+    local root = ui.findRoot(button, sidebar)
+
+    local createCollapse = function(element)
+        children = Instance.new("Frame", button)
+        local format = Instance.new("UIListLayout", children)
+        local fitChildren = function(element, size)
+            for i,v in next, element:GetChildren() do
+                if not v:IsA("UIListLayout") then
+                    element[size] = element[size] + UDim2.new(0, 0, 0, v.Size.Y.Offset)
+                end
+            end
+        end
+
+        children.BackgroundTransparency = 1
+        children.Position = UDim2.new(0, 10, 0, 20) -- To put it directly under the button, left shift for clearance
+        children.Size = UDim2.new(0, 165, 0, 0)
+        children.Name = "Children"
+
+        format.SortOrder = "LayoutOrder"
+
+        element.Collapse.Visible = true
+        element.Collapse.ClipsDescendants = false
+        element.Collapse.MouseButton1Click:Connect(function()
+            local e = element.Collapse
+            local flag = e.ClipsDescendants
+            e.Image = "rbxassetid://" .. collapseIcon[tostring(flag)]
+
+            if flag then
+                button.Size = button.Size - children.Size
+                children.Size = UDim2.new(0, 165, 0, 0)
+                --root.Size = root.Size - UDim2.new(0, 0, 0, button.Size.Y.Offset - 20) -- Collapse
+            else
+                fitChildren(children, "Size")
+                button.Size = button.Size + children.Size
+                --root.Size = root.Size + UDim2.new(0, 0, 0, button.Size.Y.Offset) -- Open 
+            end
+
+            --root.Size = root.Size + UDim2.new(0, 0, 0, button.Size.Y.Offset)
+            
+            sidebar.CanvasSize = UDim2.new(0, 0, 0, 0)
+            fitChildren(sidebar, "CanvasSize")
+
+            element.ClipsDescendants = flag
+            e.ClipsDescendants = not flag
+        end)
+
+        sidebar.CanvasSize = UDim2.new(0, 0, 0, 0)
+        fitChildren(sidebar, "CanvasSize")
+    end 
+
+    if showCollapse then
+        createCollapse(button)
+    end
+
+    if dataType == 'table' and not isEmpty(data) then
+        if not showCollapse then
+            createCollapse(button)
+        end
+        
+        local cache = {}
+        for i,v in next, data do
+            if not cache[v] then
+                ui.addButton(i, v, button.Children)
+                cache[v] = true
+            end
+        end
+    elseif dataType == 'function' and islclosure(data) and not isEmpty(getupvalues(data)) then
+        if not showCollapse then
+            createCollapse(button)
+        end
+
+        --[[local upvalues = ]]ui.addButton("Upvalues", getupvalues(data), button.Children, true)
+        --[[for i,v in next, getupvalues(data) do
+            ui.addButton(i, v, upvalues.Children)
+        end]]
+    end
+
+    if getrawmetatable(data) and not isObject(data) then
+        if not button.Collapse.Visible then
+            createCollapse(button)
+        end
+
+        local metatable = ui.addButton("Metatable", getrawmetatable(data), button.Children)
+    end
+
+    return button
+end
+
+-- < Runtime >
+interface.Parent = game.CoreGui
+
+--ui.msg("Welcome to Hydroxide", "forever better than racist dolphin's shit console")
+
+ui.addButton("debug", debug)
+ui.addButton("_G", getrenv()._G)
