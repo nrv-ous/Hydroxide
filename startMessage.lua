5563779bfb668c144c91686b4e8d18c7066549c8
nerve <38772631+0x90-NOP@users.noreply.github.com>
2019-06-15 13:24:23 -0700

    Update startMessage.lua

--- startMessage.lua
+++ startMessage.lua
@@ -1,5 +1,4 @@
 return {
   "Thanks for using Hydroxide!",
-  [[Please consider the fact that this project is not finished; It WILL have bugs.
-  Make sure to check out the change logs! (scroll down on welcome page)]]
+  "If you'd like to, feel free to join our Discord server!\nhttps://discord.gg/aVyU6DE"
 }
