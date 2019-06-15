dce92a8f20c4f360bbe1fc83a9b89ad972580e4c
nerve <38772631+0x90-NOP@users.noreply.github.com>
2019-06-15 10:03:51 -0700

    Update startMessage.lua

--- startMessage.lua
+++ startMessage.lua
@@ -1,4 +1,4 @@
 return {
   "Thanks for using Hydroxide!",
-  "Please consider the fact that this project is not finished!; It WILL have bugs.\nMake sure to check out the change logs! (scroll down)"
+  "Please consider the fact that this project is not finished; It WILL have bugs.\nMake sure to check out the change logs! (scroll down)"
 }
