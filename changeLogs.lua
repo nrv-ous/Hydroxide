f14f7d592608c9fd16c38c7906382c9330f95e20
nerve <38772631+0x90-NOP@users.noreply.github.com>
2019-06-15 20:11:19 -0700

    Update changeLogs.lua

--- changeLogs.lua
+++ changeLogs.lua
@@ -1,5 +1,6 @@
 return {
   "Made function constants viewable",
   "You can now view data information!",
-  'Added "Search Upvalues" :))'
+  'Added "Search Upvalues" :))',
+  'You can now add upvalues from "Search Upvalues" to the explorer'
 }
