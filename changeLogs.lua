b12d1951326057131e7bb2d9422ca56a5f05b014
nrv-ous <38772631+nrv-ous@users.noreply.github.com>
2019-10-06 09:17:47 -0700

    Delete changeLogs.lua

--- changeLogs.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "Made function constants viewable",
-  "You can now view data information!",
-  'Added "Search Upvalues" :))',
-  'You can now add upvalues from "Search Upvalues" to the explorer'
-}
