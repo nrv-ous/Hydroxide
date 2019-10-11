1ff6105f1dbebb143b52227eb855d39072b26278
nrv-ous <38772631+nrv-ous@users.noreply.github.com>
2019-10-10 18:50:32 -0700

    Update visuals.lua

--- visuals.lua
+++ visuals.lua
@@ -8,37 +8,45 @@ local extensions = body.Extensions
 local tabs = body.Tabs
 
 close.MouseEnter:Connect(function()
-    tween_service:Create(close, TweenInfo.new(0.10), {BackgroundColor3 = Color3.fromRGB(200, 0, 0)}):Play()
+    local animation = tween_service:Create(close, TweenInfo.new(0.10), {BackgroundColor3 = Color3.fromRGB(200, 0, 0)})
+	animation:Play()
 end)
 
 close.MouseEnter:Connect(function()
-    tween_service:Create(close, TweenInfo.new(0.10), {BackgroundColor3 = Color3.fromRGB(50, 50, 50)}):Play()
+    local animation = tween_service:Create(close, TweenInfo.new(0.10), {BackgroundColor3 = Color3.fromRGB(50, 50, 50)})
+	animation:Play()
 end)
 
 close.MouseButton1Down:Connect(function()
-    tween_service:Create(close, TweenInfo.new(0.10), {BackgroundColor3 = Color3.fromRGB(200, 100, 100)}):Play()
+    local animation = tween_service:Create(close, TweenInfo.new(0.10), {BackgroundColor3 = Color3.fromRGB(200, 100, 100)})
+	animation:Play()
 end)
 
 close.MouseButton1Up:Connect(function()
-    tween_service:Create(close, TweenInfo.new(0.10), {BackgroundColor3 = Color3.fromRGB(50, 50, 50)}):Play()
+    local animation = tween_service:Create(close, TweenInfo.new(0.10), {BackgroundColor3 = Color3.fromRGB(50, 50, 50)})
+	animation:Play()
 end)
 
 for i,v in next, extensions:GetChildren() do
     if v:IsA("TextButton") then
         v.MouseEnter:Connect(function()
-            tween_service:Create(v, TweenInfo.new(0.10), {BackgroundColor3 = Color3.fromRGB(80, 80, 80)}):Play()
+            local animation = tween_service:Create(v, TweenInfo.new(0.10), {BackgroundColor3 = Color3.fromRGB(80, 80, 80)})
+	        animation:Play()
         end)
 
         v.MouseLeave:Connect(function()
-            tween_service:Create(v, TweenInfo.new(0.10), {BackgroundColor3 = Color3.fromRGB(50, 50, 50)}):Play()
+            local animation = tween_service:Create(v, TweenInfo.new(0.10), {BackgroundColor3 = Color3.fromRGB(50, 50, 50)})
+	        animation:Play()
         end)
 
         v.MouseButton1Down:Connect(function()
-            tween_service:Create(v, TweenInfo.new(0.10), {BackgroundColor3 = Color3.fromRGB(120, 120, 120)}):Play()
+            local animation = tween_service:Create(v, TweenInfo.new(0.10), {BackgroundColor3 = Color3.fromRGB(120, 120, 120)})
+	        animation:Play()
         end)
 
         v.MouseButton1Up:Connect(function()
-            tween_service:Create(v, TweenInfo.new(0.10), {BackgroundColor3 = Color3.fromRGB(50, 50, 50)}):Play()
+            local animation = tween_service:Create(v, TweenInfo.new(0.10), {BackgroundColor3 = Color3.fromRGB(50, 50, 50)})
+	        animation:Play()
         end)
     end
 end
