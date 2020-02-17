cebbc13b4d67d7ad1f0abc41aab5da232062fb7a
vinidalvino <vinidalvino@cock.li>
2020-02-16 21:33:39 -0500

    made an base table for the gui component

--- theme-engine.lua
+++ theme-engine.lua
@@ -1,9 +1,18 @@
 local HttpService = game:GetService("HttpService")
 
+oh.GuiPart = { -- what gui component I want to theme
+    IconComponent = {
+
+    },
+    TextComponent =  {
+
+    },
+    MOTDComponent
+}
 local themeEngine = {}
 
 function themeEngine.IsUserIsHavingThemeFile()
-    if isFile('Hydroxide/theme.json') then 
+    if isFile('Hydroxide/theme.json') then
         return true
     elseif readfile('Hydroxide/theme.json') == '' then -- Hey it's tard proof!
         writefile( -- Will use hexadecimal color value instead of RGB
